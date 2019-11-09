bd369b395993e7c795ef3dc5d79489b9e03c201d
lasni <grega.lasnibat@gmail.com>
2019-11-09 15:31:05 +0100

    finishing context lesson

--- stephen_grider/translate/src/components/App.js
+++ stephen_grider/translate/src/components/App.js
@@ -1,28 +1,22 @@
 import React from 'react';
 import UserCreate from './UserCreate';
-import LanguageContext from '../contexts/LanguageContext';
+import { LanguageStore } from '../contexts/LanguageContext';
 import ColorContext from '../contexts/ColorContext';
 import LanguageSelector from './LanguageSelector';
 
 class App extends React.Component {
-  state = {
-    language: 'english'
-  }
+
   render() {
     return (
       <div className="ui container">
-        <LanguageSelector onLanguageChange={this.onLanguageChange}/>
-        <LanguageContext.Provider value={this.state.language}>
-          <ColorContext.Provider value='red'>
-            <UserCreate />
-          </ColorContext.Provider>
-        </LanguageContext.Provider>
+        <LanguageStore>
+          <LanguageSelector />
+            <ColorContext.Provider value='red'>
+              <UserCreate />
+            </ColorContext.Provider>
+        </LanguageStore>
       </div>
     )
-  }
-
-  onLanguageChange = (language) => {
-    this.setState({ language })
   }
 }
 
